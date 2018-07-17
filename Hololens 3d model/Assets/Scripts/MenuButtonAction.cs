71971f7229a3ef7f57e24016bf39dd298e6b6060
Daragh <C00195564@itcarlow.ie>
2018-07-17 16:56:15 +0100

    Added Main Menu Scene

---
+++ Hololens 3d model/Assets/Scripts/MenuButtonAction.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.Receivers;
+using HoloToolkit.Unity.InputModule;
+
+public class MenuButtonAction : InteractionReceiver
+{
+
+    protected override void InputDown(GameObject obj, InputEventData eventData)
+    {
+    }
+
+    protected override void InputUp(GameObject obj, InputEventData eventData)
+    {
+    }
+
+    protected override void InputClicked(GameObject obj, InputClickedEventData eventData)
+    {
+        //Debug.Log(obj.name + " : InputClicked");
+        switch (obj.name)
+        {
+            case "PartBuilderButton":
+                Debug.Log("PB PUSH");
+                break;
+            case "ScannerButton":
+                Debug.Log("Scanner push");
+                break;
+        }
+    }
+}
